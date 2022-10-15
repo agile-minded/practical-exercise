b80d2f0f60c57857eaf29348d385bc1ebb9506c5
agile-minded <zebubba@gmail.com>
2022-10-15 14:51:46 +1300

    Created Spring Boot starter project

---
+++ src/test/java/nz/co/vector/users/UsersApplicationTests.java
@@ -0,0 +1,13 @@
+package nz.co.vector.users;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
